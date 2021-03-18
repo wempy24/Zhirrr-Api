9bca561ec200582b99b09506f9c09c0deaae7422
ウェンピー <74690366+wempy24@users.noreply.github.com>
2021-03-18 12:58:22 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -41,7 +41,7 @@ loghandler = {
         creator: `${creator}`,
         code: 406,
         message: 'masukan parameter apikey',
-        getApikey: 'gak punya apikey? chat gw aja yaaa di https://wa.me/6285746897368 , key nya gratis kok gan, jadi santuyy ajaa'
+        getApikey: 'gak punya apikey? chat gw aja yaaa di wa.me/6285746897368 , key nya gratis kok gan, jadi santuyy ajaa'
     },
     notkey: {
         status: false,
